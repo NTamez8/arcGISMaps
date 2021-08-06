6a4fab3222f727bd35e1be57dd33a6f63dbd0b5d
nicolas <nicolas.tamez@icloud.com>
2021-08-06 11:45:07 -0500

    changed so that it uses pug files instead of html

--- index.js
+++ index.js
@@ -5,36 +5,45 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
+app.set('view engine','pug')
+
+const url = 'http://localhost:8080'
+
 
 app.get('/layers',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Pages/layers.html'))
+    //res.sendFile(path.join(__dirname,'Pages/layers.html'))
+    res.render(path.join(__dirname,'Pages','PugFiles','layers'))
 })
 
 app.get('/2dmap',(req,res)=>{
     
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.render(path.join(__dirname,'index.pug'))
 })
 app.get('/3dmap',(req,res)=>{
-    res.end("3dmap")
+    //res.end("3dmap")
 })
 
 app.get('/',(req,res)=>{
     res.redirect('/2dmap')
 })
 app.get('/widget',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Pages/widget.html'))
+    //res.sendFile(path.join(__dirname,'Pages/widget.html'))
+    res.render(path.join(__dirname,'Pages','PugFiles','widget'))
 })
 
 app.get('/featureFromGraphic',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Pages','featureFromGraphic.html'))
+    //res.sendFile(path.join(__dirname,'Pages','featureFromGraphic.html'))
+    res.render(path.join(__dirname,'Pages','PugFiles','featureFromGraphic'))
 })
 
 app.get('/mapFromCsv',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Pages','mapFromCsv.html'))
+   // res.sendFile(path.join(__dirname,'Pages','mapFromCsv.html'))
+   res.render(path.join(__dirname,'Pages','PugFiles','mapFromCsv'))
 })
 
 app.get('/census',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Pages','census.html'))
+    //res.sendFile(path.join(__dirname,'Pages','census.html'))
+    res.render(path.join(__dirname,'Pages','PugFiles','census'))
 })
 
 
