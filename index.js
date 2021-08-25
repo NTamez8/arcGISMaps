22dff7b1ae46911c923ff1c012727d20a1a8a600
nicolas <nicolas.tamez@icloud.com>
2021-08-25 15:34:32 -0400

    added dynamic url support

--- index.js
+++ index.js
@@ -1,68 +1,124 @@
 require('dotenv').config()
-
+const aws = require('aws-sdk')
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static("public"))
-app.set('view engine','pug')
+app.set('view engine', 'pug')
 
-const url = process.env.NODE_ENV != 'test' ? '<Insert base path for site>': 'http://localhost:8080'
 
-app.get('/',(req,res)=>{
-    res.redirect('/2dmap')
+let region = 'us-east-2'
+
+aws.config = {
+    region
+}
+
+const ec2 = new aws.ec2()
+
+ec2.describeInstances({
+    InstanceIds: ['']
+}, function (err, data) {
+
+    var url =  'http://localhost:8080'
+    if (err) {
+        console.log(err)
+    } else if (process.env.NODE_ENV == 'test') {
+        url =  'http://localhost:8080'
+
+    } else {
+        let temp = data.Reservations[0].Instances[0].PublicIpAddress
+        temp = temp.split('.').join('-')
+        url = `http://ec2-${temp}.${region}.compute.amazonaws.com`
+    }
+
+
+    app.get('/', (req, res) => {
+        res.redirect('/2dmap')
+    })
+
+    app.get('/layers', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'layers'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/2dmap', (req, res) => {
+
+        res.render(path.join(__dirname, 'index.pug'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+
+
+    app.get('/widget', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'widget'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/featureFromGraphic', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'featureFromGraphic'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/mapFromCsv', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'mapFromCsv'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/census', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'census'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/usa', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'usa'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/sketch', (req, res) => {
+
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'sketch'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+    app.get('/crime', (req, res) => {
+        res.render(path.join(__dirname, 'Pages', 'PugFiles', 'crime'), {
+            url,
+            "loadScript":`buildNavBar('${url}')`
+        })
+    })
+
+
+
+
+    app.listen(8080, () => console.log("listening on port 8080"))
+
+
 })
-
-app.get('/layers',(req,res)=>{
-
-    res.render(path.join(__dirname,'Pages','PugFiles','layers'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/2dmap',(req,res)=>{
-    
-    res.render(path.join(__dirname,'index.pug'),{"url":`buildNavBar('${url}')`})
-})
-
-
-
-app.get('/widget',(req,res)=>{
- 
-    res.render(path.join(__dirname,'Pages','PugFiles','widget'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/featureFromGraphic',(req,res)=>{
-   
-    res.render(path.join(__dirname,'Pages','PugFiles','featureFromGraphic'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/mapFromCsv',(req,res)=>{
-  
-   res.render(path.join(__dirname,'Pages','PugFiles','mapFromCsv'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/census',(req,res)=>{
-    
-    res.render(path.join(__dirname,'Pages','PugFiles','census'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/usa',(req,res)=>{
-   
-    res.render(path.join(__dirname,'Pages','PugFiles','usa'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/sketch',(req,res)=>{
-  
-    res.render(path.join(__dirname,'Pages','PugFiles','sketch'),{"url":`buildNavBar('${url}')`})
-})
-
-app.get('/crime',(req,res)=>{
-    res.render(path.join(__dirname,'Pages','PugFiles','crime'),{"url":`buildNavBar('${url}')`})
-})
-
-
-
-
-app.listen(8080,()=>console.log("listening on port 8080"))
