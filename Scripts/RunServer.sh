ee0c6806c3e803d2823fa35b6ca3b4d557f33f96
nicolas <nicolas.tamez@icloud.com>
2021-08-25 15:45:05 -0400

    modified scripts

--- Scripts/RunServer.sh
+++ Scripts/RunServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo systemctl start nginx
-sudo pm2 start ../index.js
+pm2 start ../index.js
--- Scripts/StopServer.sh
+++ Scripts/StopServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo systemctl stop nginx
-sudo pm2 stop ../index.js
+pm2 stop ../index.js
